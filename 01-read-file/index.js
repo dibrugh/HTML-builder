329a3ee9dc8ba67388c5ff79a8875218278b886f
dibrugh <dibrugh@gmail.com>
2023-05-08 18:21:34 +0300

    feat: solve task 1

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const path = require ('path');
+const fs = require ('fs');
+
+const { stdout } = process;
+
+const filePath = path.join(__dirname, 'text.txt');
+const readStream = fs.createReadStream(filePath, 'utf-8');
+readStream.on('data', chunk => stdout.write(chunk));
