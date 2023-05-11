847bc4ba090e2d3543a0d037f446b08b35f98d01
dibrugh <dibrugh@gmail.com>
2023-05-11 16:37:28 +0300

    fix: now copying updates folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,23 +5,41 @@ const fs = require('fs');
 const folderPath = path.join(__dirname, 'files');
 const folderCopiedPath = path.join(__dirname, 'files-copy');
 
+const copyDir = () => {
+    fs.stat(folderCopiedPath, (err) => {
+        if (!err) {
+            fs.rm(folderCopiedPath, { recursive: true }, (err) => {
+                if (err) throw err;
+                copyFiles();
+            });
+        } else {
+            copyFiles();
+        }
+    });
+}
 
 
-fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
-    if (err) {
-        throw err;
-    }
-    fs.mkdir(folderCopiedPath, { recursive: true, force: true }, err => {
+const copyFiles = () => {
+    fs.mkdir(folderCopiedPath, { recursive: true, force: true }, (err) => {
         if (err) {
             throw err;
         }
-        for (let file of files) {
-            fs.copyFile(path.join(folderPath, file.name), path.join(folderCopiedPath, file.name), err => {
-                if (err) {
-                    throw err;
-                }
-            });
-        }
+
+        fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
+            if (err) {
+                throw err;
+            }
+
+            for (let file of files) {
+                fs.copyFile(path.join(folderPath, file.name), path.join(folderCopiedPath, file.name), (err) => {
+                    if (err) {
+                        throw err;
+                    }
+                });
+            }
+        })
+
     });
-    
-});
+}
+
+copyDir();
